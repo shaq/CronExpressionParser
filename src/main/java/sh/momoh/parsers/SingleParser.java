cf5e7955c79efd3aa35173a8d3142a5764d74d06
Shaquille Momoh <aq@momoh.sh>
2021-03-07 21:38:29 +0000

    cleanup

--- src/main/java/sh/momoh/parsers/SingleParser.java
+++ src/main/java/sh/momoh/parsers/SingleParser.java
@@ -5,7 +5,6 @@ import sh.momoh.expression.IllegalCronExpressionException;
 import sh.momoh.expression.timeunit.DayOfWeek;
 import sh.momoh.expression.timeunit.Month;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.apache.commons.lang3.math.NumberUtils.isParsable;
@@ -20,17 +19,17 @@ public class SingleParser {
         switch (fieldName) {
             case "month" -> {
                 Month month = Month.from(value);
-                return Collections.singletonList(getMonthNumber(month));
+                return List.of(getMonthNumber(month));
             }
             case "day of week" -> {
                 DayOfWeek day = DayOfWeek.from(value);
-                return Collections.singletonList(getDayNumber(day, value));
+                return List.of(getDayNumber(day, value));
             }
             default -> {
                 if (isParsable(value)) {
                     int valueInt = Integer.parseInt(value);
                     if (isWithinBounds(fieldName, valueInt)) {
-                        return Collections.singletonList(valueInt);
+                        return List.of(valueInt);
                     }
                 }
             }
